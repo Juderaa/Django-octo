de29985c4b5dbecf1885cc637a84948c48ae1bf7
Juderaa <ezejudith02@gmail.com>
2022-06-19 23:14:16 +0100

    first commit

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,18 @@
 from django.contrib.auth import get_user_model
-##from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 from django.utils import timezone
 from django.db import models
-User = get_user_model()
+
 # Create your models here.
-class Post(models.Model):
+
+class Post(models.Model):    
+    Title = models.CharField(max_length = 200, default="blog_post") 
     Text = models.TextField()
     Author = get_user_model()
     Created_date = models.DateTimeField(auto_now_add=True)
     Published_date  = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self
+        return self.Title
  
 
